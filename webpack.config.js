02000a3d6c953d0b4d82c053466fbcc26fa87d76
= <=>
2018-07-27 11:41:14 +0800

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const HtmlWebpackPluginConfig = {
-	title: 'hello,零和壹在线课堂', // html5文件中<title>部分
+	title: 'hello,webpack', // html5文件中<title>部分
 	filename: 'front.html', // 默认是index.html，服务器中设置的首页是index.html，如果这里改成其它名字，那么devServer.index改为和它一样
 	template: __dirname + '/app/index.tmpl.html', // 如果觉得插件默认生成的hmtl5文件不合要求，可以指定一个模板，模板文件如果不存在，会报错，默认是在项目根目录下找模板文件，才模板为样板，将打包的js文件注入到body结尾处
 	inject: 'body', // true|body|head|false，四种值，默认为true,true和body相同,是将js注入到body结束标签前,head将打包的js文件放在head结束前,false是不注入，这时得要手工在html中加js
@@ -18,8 +18,9 @@ module.exports = {
 	devtool: 'eval-source-map',
 	entry: __dirname + "/app/main.js", //已多次提及的唯一入口文件
 	output: {
-		path: __dirname + "/build", //打包后的文件存放的地方
-		filename: "[name].bundle.js", //打包后输出文件的文件名
+		path: __dirname + "/public", //打包后的文件存放的地方
+		filename: "bundle.js",
+		// filename: "[name].bundle.js", //打包后输出文件的文件名
 		// publicPath: "http://cdn.example.com/[hash:]/"
 	},
 	devServer: {
@@ -89,7 +90,7 @@ module.exports = {
 		// new HtmlWebpackPlugin({
 		//     template: __dirname + "/app/index.tmpl.html"//根据模板生成html
 		// }),
-		new HtmlWebpackPlugin(HtmlWebpackPluginConfig),
+		// new HtmlWebpackPlugin(HtmlWebpackPluginConfig),
 		new webpack.HotModuleReplacementPlugin() //热加载插件
 	],
 
